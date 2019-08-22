52e422efb08b460033acf1880fac224670b9245d
Atheer <QoQ1213.1417@gmail.com>
2019-08-22 09:46:31 +0300

    first commit

---
+++ python1.py
@@ -0,0 +1,26 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print{" hello , world"}
+SyntaxError: invalid syntax
+>>> ptint{9)
+SyntaxError: invalid syntax
+>>> print(9);
+9
+>>> print("hello , world! ");
+hello , world! 
+>>> if 7 >  4 ;
+SyntaxError: invalid syntax
+>>> if 6>5:
+	print("7is greater than 5"):
+		
+SyntaxError: invalid syntax
+>>>  if 6>5:
+	print("7is greater than 5");
+	
+SyntaxError: unexpected indent
+>>> if 5>2:
+	print("five isgreater than two ");
+
+	
+five isgreater than two 
+>>> 
---
+++ python_1.py
@@ -0,0 +1,10 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello , my name is Atheer Alenazi");
+hello , my name is Atheer Alenazi
+>>> if 9>8:
+	print("it is successfully");
+
+	
+it is successfully
+>>> 
