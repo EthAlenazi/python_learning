3b64c0c5a54e26230bb47d3860104daf86571b32
Atheer <QoQ1213.1417@gmail.com>
2019-08-27 15:38:58 +0300

    6day_7day

---
+++ python_6.py
@@ -0,0 +1,13 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> A=str('122')
+>>> X=int(25)
+>>> Z=float(10.5)
+>>> type(Z)
+<class 'float'>
+>>> print(Z)
+10.5
+>>> int(Z)
+10
+>>> #the 6day ...
+>>> 
---
+++ python_7.py
@@ -0,0 +1,19 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name =str("Atheer Alenazi");
+>>> print(name[0:7])
+Atheer 
+>>> print(name[-5])
+e
+>>> print(name[-4:0])
+
+>>> print (name[-1:-5])
+
+>>> print (name[-5:-1])
+enaz
+>>> print(name)
+Atheer Alenazi
+>>> print(name[7])
+A
+>>> #This is code of 7days!!
+>>> 
